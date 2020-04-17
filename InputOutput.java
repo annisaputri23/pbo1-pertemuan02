13c2e5a05c82099883489036544872d4e2aa6698
ninasyahrer <ninasyahrer07@gmail.com>
2020-04-18 07:50:47 +0800

    Buat Object scanner

--- InputOutput.java
+++ InputOutput.java
@@ -1,11 +1,8 @@
-import java.until.Scanner;
+import java.util.Scanner;
 
 public class InputOutput{
  public static void main(String [] args){
   Scanner Scanner = new Scanner(System.in);
-  String nama;
-  System.out.printl("Masukkan nama anda: ");
-  nama = scanner.nextLine();
-
+ 
  }
 }
